17bf5fe80c2aa8bc6e1bde7c4d8c3cd3cda2c18f
Gennadios <egon.sch@yandex.ru>
2021-03-02 23:46:03 +0300

    added homework8 solution

---
+++ Homework8/AssemblyOne/Motorcycle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssemblyOne
+{
+    internal class Motorcycle //internal by default
+    {
+        private void Start() => Console.WriteLine("Vroom"); // private by default, only accessible within the definition of Motorcycle
+
+        internal void InternalStart() => Console.WriteLine("Vroom (internally)");
+
+        public void PublicStart() => Console.WriteLine("Vroom (publicly)");
+
+        protected void ProtectedStart() => Console.WriteLine("Vroom (protected)");
+
+        private protected void PrivateProtectedStart() => Console.WriteLine("Vroom (private protected)");
+
+        internal protected void InternalProtectedStart() => Console.WriteLine("Vroom (internal protected)");
+
+        // everything will work here as its within the class definition (basically, within the curly braces)
+        private void ClassScopeCall()
+        {
+            Start();
+            InternalStart();
+            PublicStart();
+            ProtectedStart();
+            PrivateProtectedStart();
+            InternalProtectedStart();
+        }
+    }
+}
---
+++ Homework8/AssemblyTwo/InheritedMotorcycle2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AssemblyOne;
+
+namespace AssemblyTwo
+{
+    class InheritedMotorcycle2 : PublicMotorcycle
+    {
+        private void OutsideAssemblyScope()
+        {
+            // Start(); --- will not work, because accessible only within AssemblyOne Motorcycle class scope
+            // InternalStart(); --- will not work, because accessible only within AssemblyOne
+            PublicStart();
+            ProtectedStart();
+            // PrivateProtectedStart(); --- cannot be accessed, because it's outside of its declaring assembly
+            InternalProtectedStart();
+        }
+    }
+}
